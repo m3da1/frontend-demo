bb203d7696fa9dd8e4c86b7b294fb675e22b28ad
Mark Doh <mark.elorm.doh@gmail.com>
2021-10-22 10:25:05 +0000

    Initial demo site

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple Front Site
+
+A simple front end designed with bootstrap 5 inspired by [Traversy Media](https://www.youtube.com/watch?v=4sosXZsdy-s)
---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoibTNkIiwiYSI6ImNrdjI2aGNvcTAxbWsyb2x1eGowMTRhd2YifQ.445Z75DcDaYUkyc_EPGuig';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [5.6173, -0.2145],
+    zoom: 5
+});
